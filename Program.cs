7d3526eb5ab2c4ec730417b1bde57b419e5f460a
polinabelukova <117262235+polinabelukova@users.noreply.github.com>
2022-11-13 16:38:40 +0300

    2 num

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace ProgLang
+{
+    class Program
+    {
+        static void Main()
+        {
+            double[] x = Console.ReadLine().Split(' ').Select(x=>double.Parse(x)).ToArray();
+            double a= double.Parse(Console.ReadLine());
+            for (int i = 0; i < x.Length; i++)
+            {
+                int y = (int)x[i];
+                if (y % 2 != 0&&x[i]>a)
+                {
+                    Console.Write(x[i]+" ");
+                }
+            }
+            Array.Sort(x);
+            Console.WriteLine();
+            for(int i = 0; i < x.Length; i++)
+            {
+                int g=(int)x[i];
+                if (g % 2 == 0)
+                {
+                    Console.Write(x[i]+" ");
+                }
+            }
+
+        }
+    }
+}
